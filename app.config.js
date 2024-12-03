a8c87842dddbd0a82080b279ebd87014cab00fa5
Carsoboss <carson.karren@gmail.com>
2024-12-03 16:50:12 -0700

    cleaning up naming

--- app.config.js
+++ app.config.js
@@ -2,8 +2,8 @@ import 'dotenv/config';
 
 export default {
   expo: {
-    name: "uber",
-    slug: "uber",
+    name: "Hippo",
+    slug: "Hippo",
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
