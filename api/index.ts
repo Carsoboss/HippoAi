48b63aa518231292222619bd611f4bdc8424250e
Carsoboss <carson.karren@gmail.com>
2024-12-10 16:13:49 -0700

    added vercel file and api handling

---
+++ api/index.ts
@@ -0,0 +1,7 @@
+// api/index.ts
+const { createRequestHandler } = require('@expo/server/adapter/vercel');
+const path = require('path');
+
+module.exports = createRequestHandler({
+  build: path.join(__dirname, '../dist/server'),
+});
